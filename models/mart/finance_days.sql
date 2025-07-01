1522b1ffbad916633f4f9aaf6421d27e51a3a7bb
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2025-07-01 12:29:04 +0000

    modified finance query

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -12,12 +12,13 @@
 select
     o.date_date,
     count(o.orders_id) as transactions_count,
-    SUM(o.revenue) as total_revenue,
-    SUM(o.revenue)/COUNT(o.orders_id) as average_basket_value,
-    SUM(o.operational_margin) as operational_margin,
-    SUM(o.purchase_cost) as total_purchase_cost,
-    SUM(o.ship_cost) as total_ship_cost,
-    SUM(o.log_cost) as total_log_cost,
-    SUM(o.quantity) as total_quantity_sold
+    ROUND(SUM(o.revenue),2) as total_revenue,
+    ROUND(SUM(o.revenue)/COUNT(o.orders_id),2) as average_basket_value,
+    ROUND(SUM(o.margin),2) as margin,
+    ROUND(SUM(o.operational_margin),2) as operational_margin,
+    ROUND(SUM(o.purchase_cost),2) as total_purchase_cost,
+    ROUND(SUM(o.ship_cost),2) as total_ship_cost,
+    ROUND(SUM(o.log_cost),2) as total_log_cost,
+    ROUND(SUM(o.quantity),2) as total_quantity_sold
 from {{ ref("int_orders_operational")}} as o
 group by date_date
